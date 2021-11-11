5f91a9e41205d8f8fa09493aae2dd21f2c6f6497
JunSoo Lee <ljsay1113@naver.com>
2021-11-11 11:41:00 +0900

    초간단 블로그

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+
+@app.route("/profile")
+def profile():
+    like_foods = ["떡볶이", "궁중떡볶이", "치즈떡볶이", "라볶이"]
+    return render_template("profile.html", like_foods=like_foods)
+
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
